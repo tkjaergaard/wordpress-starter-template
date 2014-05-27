c6b1604eeda9c579045ee941ba5d9e4e77dc1118
t_kjaergaard <tkj@tkjaergaard.dk>
2014-05-27 18:26:46 +0200

    Minor updates

--- app/core/helpers.php
+++ app/core/helpers.php
@@ -1,2 +1,22 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Get thumbnail url
+|--------------------------------------------------------------------------
+|
+| The the thumnail for a post by it's id.
+| A specific size can be provided.
+|
+*/
+if( !function_exists('get_thumb_url') )
+{
+    function get_thumb_url($size='thumbnail',$id=null)
+    {
+        $id = $id ?: get_post()->ID;
+
+        $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($id), $size );
+
+        return $thumb['0'];
+    }
+}
